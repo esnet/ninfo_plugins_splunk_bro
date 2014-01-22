03b308414ba37a25bd0ab18eee75ce472320f54b
Justin Azoff <justin.azoff@gmail.com>
2014-01-22 12:18:47 -0500

    bro dhcp should not run for remote hosts

--- ninfo_plugins_splunk_bro/bro_dhcp/bro_dhcp_plugin.py
+++ ninfo_plugins_splunk_bro/bro_dhcp/bro_dhcp_plugin.py
@@ -7,6 +7,7 @@ class dhcp(SplunkBase):
     description = "Entries from the Bro DHCP Log"
     types  = ['ip','mac']
     TEMPLATE = "hoursago=48 source=*dhcp.log %s | dedup mac assigned_ip | fields  mac assigned_ip | rename assigned_ip to ip"
+    remote = False
 
     def get_macs_for_ip(self, ip):
         ret = self.get_info(ip)
