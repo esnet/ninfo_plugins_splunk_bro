c8673dea03c64d9ad61a338d0471d5cd910297fe
Vlad Grigorescu <vlad@es.net>
2018-09-05 11:58:32 -0500

    Add configuration items to bro_notice

--- ninfo_plugins_splunk_bro/bro_notice/bro_notice_plugin.py
+++ ninfo_plugins_splunk_bro/bro_notice/bro_notice_plugin.py
@@ -1,3 +1,12 @@
+"""
+Optional config section:
+
+[bro_notice]
+hoursago = 48
+source = *notice.log
+fields = src dst note msg sub
+"""
+
 from ninfo.helpers.splunk import SplunkBase
 
 class bro_notice(SplunkBase):
@@ -7,6 +16,10 @@ class bro_notice(SplunkBase):
     title = "Recent Bro notices"
     description = "Recently logged bro notices from splunk"
     types  = ['ip','ip6','hostname',"username"]
-    TEMPLATE = 'hoursago=48 source="*notice.log" %s | fields src dst note msg sub'
+    hoursago = self.config.get('bro_notice', {}).get('hoursago', "48")
+    source = self.config.get('bro_notice', {}).get('source', "*notice.log")
+    fields = self.config.get('bro_notice', {}).get('fields', "src dst note msg sub")
+    
+    TEMPLATE = 'hoursago=%s source="%s" %%s | fields %s' % (hoursago, source, fields)
 
 plugin_class = bro_notice
